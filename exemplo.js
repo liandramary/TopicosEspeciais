3761d91f6cd8b472ef5554432dd67769b3f3b170
Liandra Mary <liandra.marysouza@gmail.com>
2021-03-18 20:31:12 -0300

    Lista de Exercícios

---
+++ exemplo.js
@@ -0,0 +1,39 @@
+var variavel = 123;
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = 123.505;
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = "123";
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = true;
+console.log(variavel);
+console.log(typeof variavel);
+
+variavel = { nome: "Liandra Mary" };
+console.log(variavel);
+console.log(typeof variavel);
+
+myName  =  'Chris';
+
+function  logName ( )  {
+  console.log(myName);
+}
+
+logName( );
+
+var  myName;
+
+// ----------------------------------------
+
+console.log("123" === 123);
+
+if ("123" === 123) {
+    console.log("Igual");
+} else {
+    console.log("Diferente");
+}
